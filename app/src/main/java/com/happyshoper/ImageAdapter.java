36b6cef5c8e850e22aa2f8a86fd81cedc22d46c5
Jurgis <jurgis@npteam.lt>
2014-06-29 06:32:49 +0300

    cekiai

--- app/src/main/java/com/happyshoper/ImageAdapter.java
+++ app/src/main/java/com/happyshoper/ImageAdapter.java
@@ -8,6 +8,8 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ImageAdapter extends BaseAdapter {
     private Context context;
     private final int[] receiptValues = new int[]{R.drawable.cekis1, R.drawable.cekis2, R.drawable.cekis3};
@@ -35,8 +37,14 @@ public class ImageAdapter extends BaseAdapter {
             // set image based on selected text
             ImageView imageView = (ImageView) gridView
                     .findViewById(R.id.grid_item_image);
+            TextView shopNameTextView = (TextView) gridView.findViewById(R.id.imageShopNameTextView);
+            TextView shopAddressTextView = (TextView) gridView.findViewById(R.id.imageAddressTextView);
+            TextView shopDateTextView = (TextView) gridView.findViewById(R.id.imageDateTextView);
 
                 imageView.setImageResource(receiptValues[position]);
+                shopNameTextView.setText("Danija H&M");
+                shopAddressTextView.setText("Ozo g. 25, Vilnius");
+                shopDateTextView.setText("2014-05-06");
 
 
         } else {
