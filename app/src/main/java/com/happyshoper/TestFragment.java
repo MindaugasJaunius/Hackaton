a5b8b7420576bf0aa94a54b86f3de0ad246cc029
Jurgis <jurgis@npteam.lt>
2014-06-29 04:26:32 +0300

    advices page/anim

--- app/src/main/java/com/happyshoper/TestFragment.java
+++ app/src/main/java/com/happyshoper/TestFragment.java
@@ -3,12 +3,15 @@ package com.happyshoper;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.GridView;
@@ -60,8 +63,32 @@ public final class TestFragment extends Fragment implements OnClickListener {
             return inflater.inflate(
                     R.layout.partners, null);
         } else if(mContent.equalsIgnoreCase("advices")) {
-            return inflater.inflate(
+            layout = (LinearLayout) inflater.inflate(
                     R.layout.advices, null);
+            TextView textView1 = (TextView) layout.findViewById(R.id.adviceTextView1);
+            TextView textView2 = (TextView) layout.findViewById(R.id.adviceTextView2);
+            TextView textView3 = (TextView) layout.findViewById(R.id.adviceTextView3);
+            TextView textView4 = (TextView) layout.findViewById(R.id.adviceTextView4);
+            TextView textView5 = (TextView) layout.findViewById(R.id.adviceTextView5);
+
+            final TextView textViewContent1 = (TextView) layout.findViewById(R.id.adviceTextViewContent1);
+            final TextView textViewContent2 = (TextView) layout.findViewById(R.id.adviceTextViewContent2);
+            final TextView textViewContent3 = (TextView) layout.findViewById(R.id.adviceTextViewContent3);
+            final TextView textViewContent4 = (TextView) layout.findViewById(R.id.adviceTextViewContent4);
+            final TextView textViewContent5 = (TextView) layout.findViewById(R.id.adviceTextViewContent5);
+
+            textViewContent1.setVisibility(View.GONE);
+            textViewContent2.setVisibility(View.GONE);
+            textViewContent3.setVisibility(View.GONE);
+            textViewContent4.setVisibility(View.GONE);
+            textViewContent5.setVisibility(View.GONE);
+
+            expandAdvice(textView1, textViewContent1);
+            expandAdvice(textView2, textViewContent2);
+            expandAdvice(textView3, textViewContent3);
+            expandAdvice(textView4, textViewContent4);
+            expandAdvice(textView5, textViewContent5);
+
         } else {
             layout = (LinearLayout) inflater.inflate(
                     R.layout.home, null);
@@ -82,6 +109,28 @@ public final class TestFragment extends Fragment implements OnClickListener {
 
 
         return layout;
+    }
+
+    private void expandAdvice(final TextView textView, final TextView textViewContent) {
+        textView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (textViewContent.isShown()){
+                    slideUp(view.getContext(), textViewContent);
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewContent.setVisibility(View.GONE);
+                        }
+                    }, 300);
+
+                } else {
+                    textViewContent.setVisibility(View.VISIBLE);
+                    slideDown(view.getContext(), textViewContent);
+                }
+            }
+        });
     }
 
     @Override
@@ -105,6 +154,26 @@ public final class TestFragment extends Fragment implements OnClickListener {
 //            myIntent.putExtra("key", "some text"); // Optional parameters
 //            startActivity(myIntent);
 //        }
+    }
+    private static void slideDown(Context ctx, View v){
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_down);
+        if(a != null){
+            a.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+    private static void slideUp(Context ctx, View v){
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_up);
+        if(a != null){
+            a.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
     }
 
 }
